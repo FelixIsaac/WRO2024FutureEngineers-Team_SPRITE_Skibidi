cd7fbcb906c6ac64a504e53e1b51a6851199c8d9
felixisaac <limfelix16@gmail.com>
2024-08-31 22:26:52 +0800

    update: reflect changes in pinout and components

--- src/main.py
+++ src/main.py
@@ -1,35 +1,37 @@
 from typing import List
 import time
 import RPi.GPIO as GPIO
-from motors import DCMotor
 
 from ultrasonic_sensor import UltrasonicSensor
-from color_sensor import ColorSensor
 from imu import IMU
 from motors import DCMotor, Servo
+from encoder import Encoder
 
-SWITCH = None
+# SWITCH = None
 ULTRASONIC_SENSORS: List[UltrasonicSensor] = []
-COLOR_SENSORS: List[ColorSensor] = []
 MOTOR: DCMotor = None
 SERVO: Servo = None
 IMU_SENSOR: IMU = None
+ENCODER: Encoder = None
 
 
 def setup() -> None:
-    global MOTOR
+    global MOTOR, SERVO, IMU_SENSOR, ENCODER
 
     # Using phyiscal board pin out reference
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
 
-    # We'll figure out the pins later on
-
     ULTRASONIC_SENSORS.extend([
-        # UltrasonicSensor(16, 18),  # front
+        UltrasonicSensor(12, 10),  # front
+        UltrasonicSensor(22, 24),  # right
+        UltrasonicSensor(37, 36)   # left
     ])
 
     MOTOR = DCMotor(11, 13)
+    SERVO = Servo(32)
+    ENCODER = Encoder(16)
+    IMU_SENSOR = IMU(5, 3)
 
 
 def main() -> None:
