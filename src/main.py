7db28114995e74762f73e9d06f2de6f4e8389531
felixisaac <limfelix16@gmail.com>
2024-09-02 15:50:10 +0800

    update: use gpiozero library instead of RPI.GPIO

--- src/main.py
+++ src/main.py
@@ -1,61 +1,70 @@
 from typing import List
 import time
-import RPi.GPIO as GPIO
 
-from ultrasonic_sensor import UltrasonicSensor
-from imu import IMU
-from motors import DCMotor, Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import DistanceSensor, Motor, AngularServo
+
 from encoder import Encoder
 
-# SWITCH = None
-ULTRASONIC_SENSORS: List[UltrasonicSensor] = []
-MOTOR: DCMotor = None
-SERVO: Servo = None
-IMU_SENSOR: IMU = None
+# Assuming `pigpiod` is running
+pin_factory = PiGPIOFactory()
+
+ULTRASONIC_SENSORS: List[DistanceSensor] = []
+MOTOR: Motor = None
+SERVO: AngularServo = None
+# IMU_SENSOR: IMU = None
 ENCODER: Encoder = None
 
-
 def setup() -> None:
-    global MOTOR, SERVO, IMU_SENSOR, ENCODER
+    global ULTRASONIC_SENSORS, MOTOR, SERVO, IMU_SENSOR, ENCODER
 
-    # Using phyiscal board pin out reference
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
+    # Initialise the ultrasonic sensor with the pin factory
 
     ULTRASONIC_SENSORS.extend([
-        UltrasonicSensor(12, 10),  # front
-        UltrasonicSensor(22, 24),  # right
-        UltrasonicSensor(37, 36)   # left
+        DistanceSensor(echo="BOARD10", trigger="BOARD12", pin_factory=pin_factory),  # front
+        DistanceSensor(echo="BOARD24", trigger="BOARD22", pin_factory=pin_factory),  # right
+        DistanceSensor(echo="BOARD36", trigger="BOARD37", pin_factory=pin_factory)   # left
     ])
 
-    MOTOR = DCMotor(11, 13)
-    SERVO = Servo(32)
-    ENCODER = Encoder(16)
-    IMU_SENSOR = IMU(5, 3)
-
+    MOTOR = Motor("BOARD11", "BOARD13", pin_factory=pin_factory)
+    SERVO = AngularServo("BOARD32", pin_factory=pin_factory, max_angle=90, min_angle=-90)
 
 def main() -> None:
-    # Input -> process -> output
-    # inputs are ultasonic, colour sensor, imu, camera
+    # SERVO.angle = 0
 
-    MOTOR.forward()
-    MOTOR.start(20)
+    # for i in range(-90, 90, 30):
+    #     SERVO.angle = i
+    #     print(i)
+    #     time.sleep(1)
 
+
+    # MOTOR.forward(0.1)
+
+    # Note: for ultrasonic sensor and other devices to wait for it to become active then start the script
+
+    for us in ULTRASONIC_SENSORS:
+        print(f'Distance for US {us.pin}: {us.distance * 100:.2f} cm')
+    time.sleep(0.2)
 
 if __name__ == "__main__":
     try:
         print("Setting up")
         setup()
 
-        time.sleep(1)
-
-        # Run startup sequence: e.g. servo turning, move back etc. etc.
-
-        while True:
-            main()
+        # while True:
+        main()
     except KeyboardInterrupt:
-        print("Exiting. Cleaning up")
-        
-        time.sleep(1)
+        print("Keyboard Interrupt Ctrl-C")
 
-        GPIO.cleanup()
+    except Exception as err:
+        print(err)
+
+    finally:
+        print("Exiting. Cleaning up...")
+
+        for us in ULTRASONIC_SENSORS:
+            us.close()
+        
+        MOTOR.stop()
+        MOTOR.close()
+        SERVO.close()
